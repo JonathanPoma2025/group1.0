81d399b18176d9314a3fbdcc0bd4ba56e4737095
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-05-07 09:06:35 -0600

    seguimiento

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,16 @@
 
 <x-layout>
-    <div class="text-lime-500 font-bold text-6xl text-center">
-        <h1>Hola</h1>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+        <div class= "w-full bg-white rounded-lg shadow-xl dark:border md:mt-0 sm:max-w-md xl:p-0 place-content-center">
+            <h1 class="text-black font-bold text-6xl text-center my-12 leading-tight tracking-tight">Registrarse</h1>
         
+    
+
+        
+
+        
+         </div>
     </div>
+
 </x-layout>
