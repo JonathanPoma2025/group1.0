302ff59cb3cc5abf65759ab4a816323284fb0f80
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-05-28 08:43:49 -0600

    Hola.com

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
 <body class="bg-gray-100 flex justify-center items-center h-screen shadow-3xl">
 
     <div class="flex flex-col items-center">
-    <div class="bg-gray-200 p-8 rounded-lg shadow-md w-8 mb-4 ">
+    <div class="bg-gray-200 p-8 rounded-lg shadow-md w-8/4 mb-4 ">
       <h1 class="text-2xl font-bold mb-4 text-center">Registrarse</h1>
 
       <div class="mb-4">
