c3f10b6be918c86697c242a82f10ea5fb8d546f7
Nahum Gomez <153213020+Nahum-gmz@users.noreply.github.com>
2024-05-29 11:59:17 -0600

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,21 +9,41 @@ class UserController extends Controller
 {
     public function create(Request $request) {
         $data = $request->validate([
-            '//datoverificado' => '//reglas',
+            //'datoverificados' => 'reglas',
+            'name' => 'required',
+            'birthday'=>'required|date',
+            'email' => 'required|email|unique:users,email',
+            'password'=> 'required|min:8|max:12',
         ]);
 
         if(User::create($data)) {
             return redirect('');
         }
 
-        return back()->withErrors([]);
+        return back()->withErrors([
+            'name' => 'Tu nombre es invalido. Intente una vez mas',
+            'birthday' => 'Tu fecha de nacimineto es invalida. Intente una vez mas',
+            'email' => 'Tu correo electronico es invalido. Intente una vez mas',
+            'password' => 'Tu contraseña es invalida. Intente una vez nas',
+        ]);
+        
     }
 
-    public function login() {
+    public function login(Request $request) {
+        $data = $request->validate([
+            'email' => 'required|email|exist:users,email',
+            'password' => 'required|min:8|max:12',
+        ]);
 
+        if(Auth::attempt($credentials)) {
+            return redirect('')->with('success');
+        }
+        return back()->withErrors(['email'=>'wrong credentials', 'password' => 'Wrong password']);
     }
 
-    public function update() {
-
+    public function update(User $user) {
+        //verificar $user->validate()
+        //actualizar
+        //cambio datos 
     }
 }
--- routes/postNahum.php
+++ routes/postNahum.php
@@ -1,4 +1,10 @@
 <?php
 
+use App\Http\Controller\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [UserController::class, 'login']);
+
+Route::post('/register', [UserController::class, 'create']);
+
+Route::post('users/{$id}/edit', [UserController::class, 'update']);
