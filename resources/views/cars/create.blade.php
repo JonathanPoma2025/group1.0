09c6768b7bb70e29ef3484525d43a07be970d951
849745649 <153466847+AlexBorja131@users.noreply.github.com>
2024-05-31 11:04:36 -0600

    commit

---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,32 @@
+<x-layout title="Agregar vehiculos">
+ 
+<div class="bg-gray-200 p-8 rounded-lg shadow-lg md:w-2/4 mb-4">
+    
+    <div class="text-center">
+       <h1 class="text-2xl font-bold mb-4 text-center">Agregar Vehiculo</h1>
+    </div>
+   
+    <form action="cars/create" method="POST"> 
+        
+        <body class="flex flex-col font-bold justify-center items-center h-screen ">
+            
+            <x-input type="text" name="brand" id="brand">Marca</x-input>
+            <x-input type="text" name="brand" id="brand">Modelo</x-input>
+            <x-input type="text" name="brand" id="brand">Año</x-input>
+            <x-input type="text" name="brand" id="brand">Tipo</x-input>
+            <x-input type="text" name="brand" id="brand">Placa</x-input>
+            <x-input type="text" name="brand" id="brand">Color</x-input>
+            <x-input type="text" name="brand" id="brand">NRO.o Motor</x-input>
+           
+        </body>
+            
+        
+             <div class="text-center">
+                <button type="submit" class="bg-gray-300 text-black rounded-full p-1 px-4">agregar</button>
+            </div>
+            
+        
+    </form>
+</div>
+    
+</x-layout>
--- routes/getborja.php
+++ routes/getborja.php
@@ -1,3 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('cars/create', function () {
+    return view('cars.create');
+});
+
+Route::get('cars/calendar', function () {
+    return view('cars.calendar');
+});
