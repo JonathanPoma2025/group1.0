2401280f5925a9344f7ba0558b734e0c0743bd07
Jonathan Zelada <158068008+JonathanPoma2025@users.noreply.github.com>
2024-05-31 09:19:27 -0600

    o

--- resources/views/repairshops/clients.blade.php
+++ resources/views/repairshops/clients.blade.php
@@ -9,52 +9,60 @@
             <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
             <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
-            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
-            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
-            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
-            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
-        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
-            <p class="mt-4">Sofia Medina</p>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
             <p class="font-bold pl-2">sofia@example.com</p>
-            <p class="mt-4">Vehiculo</p>
+            <p class="mt-2">Vehiculo</p>
             <p class="font-bold pl-2">Kia Rio 2018</p>
-            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 py-6 mb-6 shadow-2xl w-5/6 ">
+            <p class="">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-2">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-2">Ultimo mantenimiento</p>
             <p class="font-bold pl-2">02/08/2023</p>
         </div>
     </div>
