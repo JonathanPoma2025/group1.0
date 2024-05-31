3b7e85efb0ed165b132d107735feee89165e3698
Jonathan Zelada <158068008+JonathanPoma2025@users.noreply.github.com>
2024-05-31 09:03:38 -0600

    h

---
+++ resources/views/repairshops/clients.blade.php
@@ -0,0 +1,61 @@
+<x-layout>
+    <div class="flex flex-col items-center justify-center">
+
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6 ">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+        <div class="bg-gray-200 rounded-[24px] pl-12 pt-4 pb-10 mb-6 shadow-2xl w-5/6">
+            <p class="mt-4">Sofia Medina</p>
+            <p class="font-bold pl-2">sofia@example.com</p>
+            <p class="mt-4">Vehiculo</p>
+            <p class="font-bold pl-2">Kia Rio 2018</p>
+            <p class="mt-4">Ultimo mantenimiento</p>
+            <p class="font-bold pl-2">02/08/2023</p>
+        </div>
+    </div>
+</x-layout>
--- routes/getjona.php
+++ routes/getjona.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('repairshops/create', function () {
     return view('repairshops.create');
 } );
+
+Route::get('repairshops/clients', function () {
+    return view('repairshops.clients');
+} );
+
+Route::get('repairshops/account', function () {
+    return view('repairshops.account');
+} );
+
