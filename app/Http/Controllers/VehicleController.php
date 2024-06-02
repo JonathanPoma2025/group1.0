023d01b0257c8a3f006eed537bfb569221572861
Valeria23909 <145813825+Valeria23909@users.noreply.github.com>
2024-06-01 20:08:50 -0600

    VehicleController update

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -7,17 +7,18 @@ use Illuminate\Http\Request;
 use App\Models\Vehicle;
 
 
-class VehicleCotroller extends Controller
+class VehicleController extends Controller
 {
 
     public function store(Request $request)
     {
         $validateddata = $request->validate([
             'car_type_id'  => 'required|exists:car_types',
-            'Brand_id' => 'required|exists:brands',
-            'model' => 'required',
-            'year'   => 'required',
-            'placa' => 'required',
+            'brand_id' => 'required|exists:brands',
+            'model' => 'required|string',
+            'year'   => 'required|interger',
+            'user_id'=> 'required|exists:user_id',
+            'placa' => 'required|string',
             
         ]);
 
@@ -25,20 +26,46 @@ class VehicleCotroller extends Controller
             return redirect('/');
         }
 
-        return back()->withErrors([]);
+        return back()->withErrors([
+            'Error al registrar el vehículo'
+        ]);
     }
 
-    public function update(Vehicle $vehicle) {
+    public function update(Request $request,Vehicle $vehicle) 
+    {
         //validate
-        //
+        $validateddata = $request->validate([
+            'car_type_id'  => 'required|exists:car_types',
+            'brand_id' => 'required|exists:brands',
+            'model' => 'required|string',
+            'year'   => 'required|interger',
+            'user_id'=> 'required|exists:user_id',
+            'placa' => 'required|string',
+
+        ]);
+
+        if ($vehicle->update($validateddata)){
+            return redirect('/')->with([
+                'Los datos del vehículo se actualizó correctamente'
+            ]);
+        }
+
+        
+        return back()->withErrors([
+            'Error al actualizar el vehículo'
+        ]);
+        
     }
-    public function delete() {
+    public function delete(Vehicle $vehicle) {
 
-        #$vehicle->features()->detach();
+        $vehicle->features()->detach();
 
-        #$vehicle->delete();
+        $vehicle->delete();
 
-        return redirect('/');
+        return redirect('/')->with([
+
+        'Se ha eliminado el vehículo'
+    ]);
     }
 
     
--- routes/postVale.php
+++ routes/postVale.php
@@ -1,3 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VehicleController;
+
+Route::patch('/cars/update/{vehicle}', [VehicleController::class, 'update']);
