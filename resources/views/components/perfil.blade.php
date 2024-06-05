c5befb47738fb5b8967e9c90c34b9b6dca2795f2
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-06-05 10:43:49 -0600

    v

--- resources/views/components/perfil.blade.php
+++ resources/views/components/perfil.blade.php
@@ -1,34 +1,9 @@
 <x-layout>
 
-    <nav class="bg-gray-400 justify-end">
-        <img src="{{ asset('images/directions_car_24dp_FILL0_wght400_GRAD0_opsz24.png') }}"alt="">
-    </nav>
-
-
-    <div class="bg-gray-100 flex flex-col justify-end items-center h-screen">
-        <div class="bg-gray-400 p-8 rounded-lg shadow-lg w-full flex justify-around">
-            <div>
-                <img src="{{ asset('Images/smart_toy_20dp_FILL1_wght400_GRAD0_opsz20.png') }}" alt="" class="w-[70px]">
-                <h1>Asistente</h1>
-            </div>
-
-            <div class>
-            <img src="{{ asset('Images/directions_car_20dp_FILL1_wght400_GRAD0_opsz20.png') }}"alt="">
-            <h1>Agregar vehiculo</h1>
-            </div>
-
-            <div class="text-center">
-            <img src="{{ asset('Images/lightbulb_20dp_FILL1_wght400_GRAD0_opsz20.png') }}"alt="">
-            <h1>Tips</h1>
-            </div>
-
-            <div class="text-center">
-            <img src="{{ asset('Images/map_20dp_FILL1_wght400_GRAD0_opsz20.png') }}"alt="">
-            <h1>Mapa</h1>
-            </div>
-
-            <h1 class="text-2xl font-bold mb-4 text-center"></h1>
+    <div class="relative">
+        <div class="bg-black flex items-center justify-between rounded-full shadow-lg p-2 m-10">
+            <img scr="{{ asset("imagenes/caricon.png")}}" alt="" class="mx-auto w-20">
         </div>
-
+    </div>
 
 </x-layout>
