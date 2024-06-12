7c788654b4e32ddeffff2167ab132399d231b839
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-06-12 10:30:20 -0600

    no se

--- resources/views/components/perfil.blade.php
+++ resources/views/components/perfil.blade.php
@@ -1,8 +1,33 @@
 <x-layout>
 
-    <div class="relative">
-        <div class="bg-black flex items-center justify-between rounded-full shadow-lg p-2 m-10">
-            <img scr="{{ asset("imagenes/caricon.png")}}" alt="" class="mx-auto w-20">
+
+    <nav class="bg-gray-400 justify-end "></nav>
+
+
+    <div class="bg-gray-100 flex flex-col justify-end items-center h-screen w-full mb-12">
+        <div class="bg-gray-300 p-5 w-3/4 shadow-lg flex justify-between rounded-full">
+
+            <div>
+                <img src="{{ asset('Images/home_20dp.png') }}" alt="" class="w-7">
+                <h1>Inicio</h1>
+            </div>
+
+            <div>
+            <img src="{{ asset('Images/icono4.png') }}"alt="" class="w-7 text-center">
+            <h1>Asistente</h1>
+            </div>
+
+            <div>
+            <img src="{{ asset('Images/icono1.png') }}"alt="" class="w-7">
+            <h1>Add car</h1>
+            </div>
+
+            <div>
+            <img src="{{ asset('Images/icono3.png') }}"alt="" class="w-7">
+            <h1>Tips</h1>
+            </div>
+
+            <h1 class="text-2xl font-bold mb-4 text-center"></h1>
         </div>
     </div>
 
