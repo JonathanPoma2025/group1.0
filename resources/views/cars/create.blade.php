2fa334bb21b646541d43aa67b5f9fde7ad117185
Valeria23909 <145813825+Valeria23909@users.noreply.github.com>
2024-06-12 10:54:40 -0600

    commit xd

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -1,18 +1,17 @@
 <x-layout title="Agregar vehiculos">
- 
+
 <div class="bg-gray-200 p-8 rounded-lg shadow-lg md:w-2/4 mb-4">
-    
+
     <div class="text-center">
        <h1 class="text-2xl font-bold mb-4 text-center">Agregar Vehiculo</h1>
     </div>
 
-   
-    <form action="cars/create" method="POST"> 
+
+    <form action="cars/store" method="POST">
         @csrf
-        <body class="flex flex-col font-bold justify-center items-center h-screen ">
-            
-          
-            <select name="select">
+        <div class="flex flex-col font-bold justify-center items-center h-screen ">
+
+            <select name="brand_id">
                 @foreach ($brands as $brand)
                     <option value="{{$brand->id}}">
                         {{$brand->name}}
@@ -20,19 +19,12 @@
                 @endforeach
             </select>
 
-            
-            <label>
             <x-input type="text" name="model" id="model">Modelo</x-input>
-            </label>
-
-           
 
             <x-input type="date" name="year" id="year">Año</x-input>
 
-
-            <select name="select">
+            <select name="car_type_id">
                 @foreach($car_types as $car_type)
-
                 <option value="{{$car_type->id}}">
                     {{$car_type->name}}
                 </option>
@@ -45,16 +37,15 @@
             <x-input type="text" name="color" id="color">Color</x-input>
 
             <x-input type="text" name="motor" id="motor">NRO.o Motor</x-input>
-           
-        </body>
-            
-        
+
+        </div>
+
              <div class="text-center">
                 <button type="submit" class="bg-gray-300 text-black rounded-full p-1 px-4">agregar</button>
             </div>
-            
-        
+
+
     </form>
 </div>
-    
+
 </x-layout>
--- routes/postVale.php
+++ routes/postVale.php
@@ -2,3 +2,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 
+Route::post('cars/store', [VehicleController::class, 'store']);
