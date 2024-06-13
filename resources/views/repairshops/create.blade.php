6a5eb7acc811aa259fc64e7da07bdc07ed68680b
Jonathan Zelada <158068008+JonathanPoma2025@users.noreply.github.com>
2024-06-13 10:30:15 -0600

    cambios

--- resources/views/repairshops/create.blade.php
+++ resources/views/repairshops/create.blade.php
@@ -1,22 +1,22 @@
+<x-layoutbg1>
+    <div class="flex flex-col justify-center items-center h-screen w-full pt-2">
+        <x-card>
+            <img src="{{ asset("images/logocar.png")}}" alt="" class="mx-auto w-32">
 
-<x-layoutbg>
-    <div class="flex flex-col justify-center items-center h-screen w-full">
-        <div class="bg-gray-200 bg-opacity-80 p-8 rounded-[20px] md:w-2/4 shadow-2xl mb-4">
-            <img src="{{ asset("imagenes/caricon.png")}}" alt="" class="mx-auto w-24">
+            <h1 class="text-xl text-gray-5 py-2 font-bold mb-4 text-center">Registra tu taller</h1>
 
-            <h1 class="text-xl py-2 font-bold mb-4 text-center">Registra tu taller</h1>
+                <div class="mb-4 px-9 flex flex-col items-center">
+                    <x-input type="" id="" name="">Nombre del taller</x-input>
 
-            <x-input type="" id="" name="">Nombre del taller</x-input>
+                    <x-input type="" id="" name="">Propietario</x-input>
 
-            <x-input type="" id="" name="">Propietario</x-input>
+                    <x-input type="" id="" name="">Dirección</x-input>
 
-            <x-input type="" id="" name="">Dirección</x-input>
+                    <x-input type="email" id="email" name="correo">Dirección de correo electrónico</x-input>
 
-            <x-input type="email" id="email" name="correo">Dirección de correo electrónico</x-input>
-
-            <x-input type="password" id="" name="contraseña">Contraseña</x-input>
-
-            <button class="mx-auto bg-gray-300 text-black rounded-full p-1 px-4 text-xs flex flex-col font-bold" type="submit">Registrarse</button>
-        </div>
+                    <x-input type="password" id="" name="contraseña">Contraseña</x-input>
+                </div>
+            <button class="mx-auto bg-ourblack text-gray-5 rounded-full p-1 px-4 text-xs flex flex-col font-bold" type="submit">Registrarse</button>
+        </x-card>
     </div>
-</x-layoutbg>
+</x-layoutbg1>
