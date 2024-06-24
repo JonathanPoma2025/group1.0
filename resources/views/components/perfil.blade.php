ae9c978554b7d967b51bb8328fc2729334ac437e
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-06-24 10:55:46 -0600

    Barra de nav

--- resources/views/components/perfil.blade.php
+++ resources/views/components/perfil.blade.php
@@ -3,7 +3,6 @@
 
     <nav class="bg-gray-400 justify-end "></nav>
 
-
     <div class="bg-gray-100 flex flex-col justify-end items-center h-screen w-full mb-12">
         <div class="bg-gray-300 p-5 w-3/4 shadow-lg flex justify-between rounded-full">
 
@@ -13,18 +12,23 @@
             </div>
 
             <div>
-            <img src="{{ asset('Images/icono4.png') }}"alt="" class="w-7">
-            <h1>Asistente</h1>
+                <img src="{{ asset('Images/icono 2.png') }}"alt="" class="w-7">
+                <h1>Asistente</h1>
             </div>
 
             <div>
-            <img src="{{ asset('Images/icono1.png') }}"alt="" class="w-7">
-            <h1>Add car</h1>
+                <img src="{{ asset('Images/icono7.png') }}"alt="" class="w-7">
+                <h1>Calendario</h1>
             </div>
 
             <div>
-            <img src="{{ asset('Images/icono3.png') }}"alt="" class="w-7">
-            <h1>Tips</h1>
+                <img src="{{ asset('Images/icono1.png') }}"alt="" class="w-7">
+                <h1>Add car</h1>
+            </div>
+
+            <div>
+                <img src="{{ asset('Images/icono3.png') }}"alt="" class="w-7">
+                <h1>Tips</h1>
             </div>
 
             <h1 class="text-2xl font-bold mb-4 text-center"></h1>
