9fbecff9d02828f4b09a0c4690293690eb1cd6ca
FlorDominguez32 <146401735+FlorDominguez32@users.noreply.github.com>
2024-07-10 09:04:37 -0600

    cambios style

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -3,20 +3,20 @@
   <div class="relative overflow-hidden w-full">
     <div class=" bg-ourblack bg-opacity-10 rounded-full h-[175%] w-[200%] absolute bottom-[60%] -left-[50%] z-0 flex justify-center items-end md:*:">
          <article class="text-nowrap flex flex-col justify-center">
-          
-            <h2 class="text-white" >Tu Proximo mantenimiento es en:</h2>
 
-           <p class="text-white principal de clientes 2xl font-bold text-center">2 meses</p>
+            <h2 class="text-white text-1xl" >Tu Proximo mantenimiento es en:</h2>
+
+           <p class="text-white principal de clientes text-3xl font-bold text-center">2 meses</p>
            <x-primary-button class="mb-20 w-40 mx-auto">Editar datos</x-primary-button>
 
-           
+
           </article>
-  
+
     </div>
-    
+
 
     <div class=" flex justify-center h-screen shadow-3xl z-40">
-      
+
     </div>
   </div>
 
