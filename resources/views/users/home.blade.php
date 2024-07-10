51c673e02722ab36cee54a94f0efb19e92fc60ed
849745649 <153466847+AlexBorja131@users.noreply.github.com>
2024-07-10 08:52:34 -0600

    user home

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,22 +1,23 @@
-<x-layout title="Inicio">
+<x-layoutbg1 title="Home clientes">
 
   <div class="relative overflow-hidden w-full">
-    <div class="bg-black rounded-full h-[175%] w-[200%] absolute bottom-[60%] -left-[50%] z-0 flex justify-center items-end">
-         <article class="text-nowrap">
+    <div class=" bg-ourblack bg-opacity-10 rounded-full h-[175%] w-[200%] absolute bottom-[60%] -left-[50%] z-0 flex justify-center items-end md:*:">
+         <article class="text-nowrap flex flex-col justify-center">
           
-          <h2 class="text-white" >Tu Proximo mantenimiento es en:</h2>
+            <h2 class="text-white" >Tu Proximo mantenimiento es en:</h2>
 
-           <p class="text-white principal de clientes 2xl font-bold text-center pb-32">2 meses</p>
+           <p class="text-white principal de clientes 2xl font-bold text-center">2 meses</p>
+           <x-primary-button class="mb-20 w-40 mx-auto">Editar datos</x-primary-button>
 
            
-          <button class="text-white">Editar datos</button>
-          
           </article>
+  
     </div>
+    
 
     <div class=" flex justify-center h-screen shadow-3xl z-40">
       
     </div>
   </div>
 
-</x-layout>
+</x-layoutbg1>
