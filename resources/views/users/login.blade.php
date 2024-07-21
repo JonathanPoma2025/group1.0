4c8674c2a603ef2735082377bacf6e73f98b1fbb
FlorDominguez32 <146401735+FlorDominguez32@users.noreply.github.com>
2024-07-21 11:51:41 -0600

    logos TrackMate

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -2,7 +2,7 @@
 
     <div class="h-screen flex items-center">
         <div class="py-12 px-20 bg-ourblack bg-opacity-30 rounded-lg drop-shadow-2xl">
-            <img src="{{asset("imagenes/caricon.png")}}" class="mx-auto w-24">
+            <img src="{{asset("images/logo 2.png")}}" class="mx-auto w-24">
             <h1 class="text-2xl font-bold my-4 text-center text-white">Iniciar sesión en TrackMate</h1>
             <form action="/login" method="POST"  class="text-center mt-6">
                 @csrf
