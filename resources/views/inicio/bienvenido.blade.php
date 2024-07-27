810cf5ec6f19c8e909d31e17025aee719d67833d
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-07-26 20:28:26 -0600

    Welcome

--- resources/views/inicio/bienvenido.blade.php
+++ resources/views/inicio/bienvenido.blade.php
@@ -1,17 +1,30 @@
 <x-layoutbg1>
-<x-card class="scroll-full h-full">
-<img src="{{ asset('Images/Logo 2.png') }}" alt="" class="w-20 mx-auto md:'12px'">
-    <p class="Ubuntu-fold text-center text-1xl ">TrackMate</p>
-    <article class="align-baseline text-center">
+    <div class="bg-card bg-opacity-30 py-8 my-10 text-white text-center px-6 w-4/5 mx-auto rounded-[20px] md:w-2/4 drop-shadow-2xl shadow-2xl">
+
+    <img src="{{ asset('Images/Logo 2.png') }}" alt="" class="w-20 mx-auto md:'12px '">
+    <p class="Ubuntu-bold 2xl">TrackMate</p>
+    <article class="align-baseline">
         <h1 class="text-xl font-bold mt-6">Bienvenido/a</h1>
         <p class="mt-6">Somos una App cuyo proposito es ayudar con el mantenimiento y gestion automotriz de tu vehiculo,
             contribuyendo al cuidado de el.
         </p>
+
+    <div>
+        <img src="{{ asset('Images/circle 1.png') }}" alt="" class="w-7 mt-8 ml-48">
+    </div>
+
+    <div>
+        <img src="{{ asset('Images/circle.png') }}" alt="" class="w-6">
+    </div>
+
+        <x-primary-button class="mb-20 w-40 mx-auto">registrarse</x-primary-button>
+
+        <h1>¿ya tienes una cuenta? inicia sesion</h1>
+
+
+
     </article>
-    <img src="{{ asset('Images/circle 1.png') }}" alt="" >
-    <img src="{{ asset('Images/circle.png') }}" alt="" class="w-6 ml-72 mb-6 my-8 ">
+    </div>
+    </x-layoutbg1>
 
-    <x-primary-button>registrarse</x-primary-button>
 
-</x-card>
-</x-layoutbg1>
