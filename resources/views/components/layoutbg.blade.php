eaca3f40b554c20323373a2ffd7c9a8ee7421969
FlorDominguez32 <146401735+FlorDominguez32@users.noreply.github.com>
2024-08-13 10:02:15 -0600

    commit new

--- resources/views/components/layoutbg.blade.php
+++ resources/views/components/layoutbg.blade.php
@@ -8,5 +8,7 @@
 </head>
 <body class="bg-gray-100 flex flex-col justify-center items-center" style="background-image: url('{{ asset("imagenes/fondo.png") }}'); background-size: cover; background-position: center center;">
     {{$slot}}
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
 </body>
 </html>
--- resources/views/inicio/bienvenido.blade.php
+++ resources/views/inicio/bienvenido.blade.php
@@ -1,17 +1,25 @@
 <x-layoutbg1>
-<x-card class="scroll-full h-full">
-<img src="{{ asset('Images/Logo 2.png') }}" alt="" class="w-20 mx-auto md:'12px'">
-    <p class="Ubuntu-fold text-center text-1xl ">TrackMate</p>
-    <article class="align-baseline text-center">
-        <h1 class="text-xl font-bold mt-6">Bienvenido/a</h1>
-        <p class="mt-6">Somos una App cuyo proposito es ayudar con el mantenimiento y gestion automotriz de tu vehiculo,
-            contribuyendo al cuidado de el.
-        </p>
-    </article>
-    <img src="{{ asset('Images/circle 1.png') }}" alt="" >
-    <img src="{{ asset('Images/circle.png') }}" alt="" class="w-6 ml-72 mb-6 my-8 ">
+    <swiper-container class="w-100 h-100" space-between="50" history-key="slide" pagination="true" navigation="true">
+        <swiper-slide>
+            <x-card class="scroll-full h-full">
+                <img src="{{ asset('Images/Logo 2.png') }}" alt="" class="w-20 mx-auto md:'12px'">
+                <p class="Ubuntu-fold text-center text-1xl ">TrackMate</p>
+                <article class="align-baseline text-center">
+                    <h1 class="text-xl font-bold mt-6">Bienvenido/a</h1>
+                    <p class="mt-6">Somos una App cuyo proposito es ayudar con el mantenimiento y gestion automotriz
+                        de tu vehiculo,
+                        contribuyendo al cuidado de el.
+                    </p>
+                </article>
+                <div class="w-6">
+                    <img src="{{ asset('Images/circle 1.png') }}" alt="" class="text-left">
+                    <img src="{{ asset('Images/circle.png') }}" alt="">
+                </div>
+                <x-primary-button>registrarse</x-primary-button>
 
-    <x-primary-button>registrarse</x-primary-button>
-
-</x-card>
+            </x-card>
+        </swiper-slide>
+        <swiper-slide data-history="Slide 2">Slide 2</swiper-slide>
+        <swiper-slide data-history="3">Slide 3</swiper-slide>
+    </swiper-container>
 </x-layoutbg1>
