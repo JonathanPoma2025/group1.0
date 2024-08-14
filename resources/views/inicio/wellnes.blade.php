8f9318fdea33ac3a2fe66634162d267caa62f69f
849745649 <153466847+AlexBorja131@users.noreply.github.com>
2024-08-14 08:16:07 -0600

    .

--- resources/views/inicio/wellnes.blade.php
+++ resources/views/inicio/wellnes.blade.php
@@ -1,3 +1,33 @@
 <x-layoutbg1>
 
+<<<<<<< Updated upstream
 </x-layoutbg1>
+=======
+<<<<<<< HEAD
+<img src="{{ asset('Images/Logo 2.png') }}" alt="" class="w-20 mx-auto md:'12px '">
+<p class="Ubuntu-bold 2xl">TrackMate</p>
+<article class="align-baseline">
+    <h1 class="text-xl font-bold mt-6">Bienestar y seguridad</h1>
+    <p class="mt-6">Cuida facilmente de tu vehiculo y mantenlo seguro para evitar problemas
+    </p>
+    
+    <div class="flex justify-center mt-8 items-center">
+        <img src="{{ asset('Images/circle 1.png') }}" alt="" class="w-5 invisible">
+        <img src="{{ asset('Images/circle.png') }}" alt="" class="w-5">
+        <img src="{{ asset('Images/circle.png') }}" alt="" class="w-5">
+        <img src="{{ asset('Images/circle.png') }}" alt="" class="w-5">
+        <img src="{{ asset('Images/circle 1.png') }}" alt="" class="w-5">
+    </div>
+
+    <h1>¿ya tienes una cuenta? inicia sesion</h1>
+    
+    <x-primary-button class="mb-20 w-40 mx-auto">registrarse</x-primary-button>
+    
+    
+</article>
+</div>
+</x-layoutbg1> 
+=======
+</x-layoutbg1>
+>>>>>>> 466846ea48d11085aeafecef42bbecb6815f80f9
+>>>>>>> Stashed changes
