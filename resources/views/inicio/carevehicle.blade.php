234cb11d6c33ac9471313fc12706150ec315309f
Fernando Alfaro :) <145823353+23ed33@users.noreply.github.com>
2024-08-14 09:02:05 -0600

    Solucion

--- resources/views/inicio/carevehicle.blade.php
+++ resources/views/inicio/carevehicle.blade.php
@@ -8,6 +8,15 @@
         <p class="mt-6">consejos y herramientas para el mantenimientode tu auto y recomendaciones para mantenerlo
             en buen estado
         </p>
+
+        <div class="flex justify-center mt-8 items-center">
+            <img src="{{ asset('Images/circle 1.png') }}" alt="" class="rotate-180 w-8">
+            <img src="{{ asset('Images/circle.png') }}" alt="" class="w-4 ml-5">
+            <img src="{{ asset('Images/circle.png') }}" alt="" class="w-4 ml-5">
+            <img src="{{ asset('Images/circle.png') }}" alt="" class="w-4 ml-5">
+            <img src="{{ asset('Images/circle 1.png') }}" alt="" class="w-8 ml-8">
+        </div>
+
         <x-primary-button class="mb-20 w-40 mx-auto">registrarse</x-primary-button>
 
         <h1>¿ya tienes una cuenta? inicia sesion</h1>
@@ -15,4 +24,4 @@
 
     </article>
     </div>
-    </x-layoutbg1> 
+    </x-layoutbg1>
