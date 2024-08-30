7100fe26dc603d610ab937e7b2082c74d46e0654
849745649 <153466847+AlexBorja131@users.noreply.github.com>
2024-08-30 08:13:41 -0600

    cambios de viewclient

--- resources/views/users/viewclient.blade.php
+++ resources/views/users/viewclient.blade.php
@@ -1,1 +1,29 @@
-<h1>HOLA</h1>
+<x-layoutbg1>
+        <img src="{{ asset('Images/contac.png') }}" alt="" class="absolute top-0 right-0 h-12 w-12">
+
+
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
+                <p class="font-bold text-center text-2xl">Taller los amigos</p>
+                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center right-11">
+
+
+        </div>
+
+
+
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
+                <p class="font-bold text-center text-2xl ">Taller santa cruz</p>
+                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
+
+
+        </div>
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
+                <p class="font-bold text-center text-2xl ">Taller los hermanos</p>
+                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
+
+
+
+        </div>
+
+
+</x-layoutbg1>
