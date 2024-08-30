9df9ab6d725a26ee52f5602f5593a9c0075dc49f
849745649 <153466847+AlexBorja131@users.noreply.github.com>
2024-08-30 08:44:04 -0600

    para que te enamore more

--- resources/views/users/viewclient.blade.php
+++ resources/views/users/viewclient.blade.php
@@ -2,28 +2,36 @@
         <img src="{{ asset('Images/contac.png') }}" alt="" class="absolute top-0 right-0 h-12 w-12">
 
 
-        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
-                <p class="font-bold text-center text-2xl">Taller los amigos</p>
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl flex justify-center items-center pb-6 text-gray-5">
                 <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center right-11">
+                <p class="font-bold text-center text-2xl">Taller los amigos</p>
+        </div>
 
+        <div class="flex justify-center mt-8 items-center">
+
+                <p class="font-bold pl-2">Mecanico</p>
+                <p class="mt-2">Juan Perez</p>
 
         </div>
 
 
-
-        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl flex justify-center items-center pb-6 text-gray-5">
+                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
                 <p class="font-bold text-center text-2xl ">Taller santa cruz</p>
-                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
-
-
         </div>
-        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl text-xs pb-6 text-gray-5">
+
+
+
+        <div class="rounded-[24px] px-6 py-6 mt-12 bg-ourblack bg-opacity-30 w-3/4 md:w-2/4 shadow-2xl flex justify-center items-center pb-6 text-gray-5">
+                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
                 <p class="font-bold text-center text-2xl ">Taller los hermanos</p>
-                <img src="{{ asset('Images/circle.png') }}" class="flex justify-center items-center">
-
-
-
         </div>
+
+
+
+
+
+
 
 
 </x-layoutbg1>
